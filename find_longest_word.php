181de2a24c2ac116acb99b1f4a1d7300aff5fa76
thecodeliner <dropsaill@gmail.com>
2025-06-01 23:44:42 +0600

    add commnents

--- find_longest_word.php
+++ find_longest_word.php
@@ -6,13 +6,16 @@
         
         //split the sentence to words
         $words = explode(' ', $replaceChar);
-        
+        // define a variable to hold the longest word
         $max = '';
+        //define an array to hold the longest word
         $longestWord = [];
+        //loop to find the longest word
         foreach ($words as $key => $word){
             if (strlen($word) > strlen($max)){
                 $max = $word;
                 $longestWord = [$key => $word];
+                
             }elseif (strlen($word) == strlen($max)){
                 $longestWord[$key] = $word;
             }
--- wordcount.php
+++ wordcount.php
@@ -3,12 +3,15 @@
 <?php
 
 function wordCount($sentence) {
-
+    //lowercase the sentence 
     $lowerCase = strtolower($sentence);
+    //remove special characters from the sentence
     $replaceChar = str_replace(['!', '?', '.', ','], '', $lowerCase);
+    //split the sentence to words
     $words = explode(' ', $replaceChar);
+    //count the values of each word
     $wordCount = array_count_values($words);
-
+    //return the word count
     return $wordCount;
     
 }
