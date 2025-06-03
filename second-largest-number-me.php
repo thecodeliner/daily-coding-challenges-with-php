5427c4af69c835bfe5fe6d657e5c25a3fa09f635
thecodeliner <dropsaill@gmail.com>
2025-06-04 00:53:07 +0600

    add first commit

--- second-largest-number-me.php
+++ second-largest-number-me.php
@@ -1,10 +1,14 @@
 <?php
 
 	$numbers = [11,25,54,36,78,98,120];
-    
+  
+    //sort the array in descending order
     arsort($numbers);
+    //remove the first element (largest number)
     array_shift($numbers);
+    //initialize the largest variable
     $largest = 0;
+    //find the second largest number
     foreach ($numbers as $number){
     	if ($number > $largest){
         	$largest = $number;
